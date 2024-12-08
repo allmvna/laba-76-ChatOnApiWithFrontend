fde275331934cd24fa4f2376f5b4224cf1f0c887
Begimai Mederbekova <begimaialymbekova2@gmail.com>
2024-12-09 01:40:16 +0600

    Added CORS support for handling cross-domain requests

--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import messagesRouter from "./routers/messages";
 import fileDb from "./fileDb";
+import cors from "cors";
 
 const app = express();
 const port = 8000;
 
+app.use(cors());
 app.use(express.json());
 app.use('/messages', messagesRouter);
 
